5e4ace58bf7cd0cd814b9483b94c26e5b4cd5837
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2020-12-30 20:57:58 -0500

    Keep git dirs

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -9,8 +9,8 @@ git clone --branch gh-pages https://$1@github.com/$GITHUB_REPOSITORY.git /gh-pag
 
 # update gh-pages branch with changes
 echo "Synching /public directory"
-find /gh-pages -type f ! -name "CNAME" ! -path "*/.git/*" -delete
-find /gh-pages -type d -depth -mindepth 1 ! -name ".git" -delete
+find /gh-pages -type f ! -name "CNAME" ! -path "*git*" -delete
+find /gh-pages -type d -depth -mindepth 1 ! -path "*git*" -delete
 mv -f $GITHUB_WORKSPACE/public/* /gh-pages
 
 # set working dir
