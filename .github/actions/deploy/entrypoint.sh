850e28420078b16804f8467023c2b15c39d7dd56
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2021-03-23 20:30:56 -0400

    Use GITHUB_TOKEN

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -5,7 +5,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 # clone gh-pages branch
-git clone --branch gh-pages https://$1@github.com/$GITHUB_REPOSITORY.git /gh-pages
+git clone --branch gh-pages https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git /gh-pages
 
 # update gh-pages branch with changes
 echo "Synching /public directory"
@@ -19,4 +19,4 @@ cd /gh-pages
 # push gh-pages branch
 git add --all
 git commit --allow-empty --message="Publish $GITHUB_SHA"
-git push --force https://$1@github.com/$GITHUB_REPOSITORY.git gh-pages
+git push --force https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git gh-pages
